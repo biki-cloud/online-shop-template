877c72960f49afea3e2d3babd6f183d85ff135da
hibiki <hibiki123123@gmail.com>
2025-03-01 13:39:56 +0900

    Refactor e2e/product.spec.ts to update test screenshot paths

--- e2e/product.spec.ts
+++ e2e/product.spec.ts
@@ -4,20 +4,20 @@ test("商品詳細ページが開けること", async ({ page }) => {
   // ページにアクセス
   await page.goto("/home");
   // スクリーンショットを撮る
-  await page.screenshot({ path: "screenshot1.png" });
+  await page.screenshot({ path: "test-results/screenshot1.png" });
   // ボタンをクリックする. getByRoleで指定する
   // 要素を取得する際はgetByRole, getByTextを使う
   await page.getByRole("button", { name: "Products" }).click();
   // 2秒待機
   await page.waitForTimeout(2000); // 2秒待機
-  await page.screenshot({ path: "screenshot2.png" });
+  await page.screenshot({ path: "test-results/screenshot2.png" });
   await page
     .getByRole("link", {
       name: "クラシック ホワイト Tシャツ クラシック ホワイト T",
     })
     .click();
   await page.waitForTimeout(2000); // 2秒待機
-  await page.screenshot({ path: "screenshot3.png" });
+  await page.screenshot({ path: "test-results/screenshot3.png" });
 
   // 画面に"クラシック ホワイト Tシャツ"が表示されていることを確認
   // アサート
