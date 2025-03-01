7c2bc67274c3bca1baccb11dc3c2d8a920c09012
hibiki <hibiki123123@gmail.com>
2025-03-01 17:03:00 +0900

    Refactor e2e/auth.spec.ts to update login form selectors and fill in the email and password fields

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,22 +1,26 @@
 import { test, expect, Page } from "@playwright/test";
 
-test("ユーザー権限でログインできること", async ({ page }) => {
+async function login(page: Page, email: string, password: string) {
   await page.goto("/sign-in");
+
   const mail_textbox = await page.getByRole("textbox", {
     name: "メールアドレス",
   });
-  await mail_textbox.click();
-  await mail_textbox.fill("test@example.com");
+  await mail_textbox.fill(email);
+
   const password_textbox = await page.getByRole("textbox", {
     name: "パスワード",
   });
-  await password_textbox.click();
-  await password_textbox.fill("password123");
+  await password_textbox.fill(password);
 
   const submit_button = await page
     .locator("form")
     .getByRole("button", { name: "サインイン" });
   await submit_button.click();
+}
+
+test("ユーザー権限でログインできること", async ({ page }) => {
+  await login(page, "test@example.com", "password123");
 
   await page.getByRole("button", { name: "User menu" }).click();
   await page.waitForTimeout(4000);
@@ -24,28 +28,10 @@ test("ユーザー権限でログインできること", async ({ page }) => {
 });
 
 test("管理者権限でログインできること", async ({ page }) => {
-  await page.goto("/sign-in");
   await page.screenshot({ path: "test-results/auth_before-sign-in.png" });
-
-  const mail_textbox = await page.getByRole("textbox", {
-    name: "メールアドレス",
-  });
-  await mail_textbox.click();
-  await mail_textbox.fill("admin@example.com");
-
-  const password_textbox = await page.getByRole("textbox", {
-    name: "パスワード",
-  });
-  await password_textbox.click();
-  await password_textbox.fill("admin123");
-
-  const submit_button = await page
-    .locator("form")
-    .getByRole("button", { name: "サインイン" });
-  await submit_button.click();
-
-  await page.waitForTimeout(4000);
+  await login(page, "admin@example.com", "admin123");
   await page.screenshot({ path: "test-results/auth_after-sign-in.png" });
+
   await page.getByRole("button", { name: "User menu" }).click();
   await expect(page.getByText("admin@example.com")).toBeVisible();
 });
