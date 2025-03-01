6421f9db57e10b03ec43081c22c35cef73a9f511
hibiki <hibiki123123@gmail.com>
2025-03-01 15:34:27 +0900

    Refactor .gitignore and playwright.config.ts

---
+++ e2e/auth.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect, Page } from "@playwright/test";
+
+test("ユーザー権限でログインできること", async ({ page }) => {
+  await page.goto("/sign-in");
+  await page.getByRole("textbox", { name: "メールアドレス" }).click();
+  await page
+    .getByRole("textbox", { name: "メールアドレス" })
+    .fill("test@example.com");
+  await page.getByRole("textbox", { name: "パスワード" }).click();
+  await page.getByRole("textbox", { name: "パスワード" }).fill("password123");
+  await page
+    .locator("form")
+    .getByRole("button", { name: "サインイン" })
+    .click();
+  await page.getByRole("button", { name: "User menu" }).click();
+  await page.waitForTimeout(4000);
+  await expect(page.getByText("test@example.com")).toBeVisible();
+});
+
+test("管理者権限でログインできること", async ({ page }) => {
+  await page.goto("/sign-in");
+  await page.screenshot({ path: "test-results/auth_before-sign-in.png" });
+  const mail_textbox = await page.getByRole("textbox", {
+    name: "メールアドレス",
+  });
+  mail_textbox.click();
+  mail_textbox.fill("admin@example.com");
+  //   const password_textbox = await page.getByRole("textbox", {
+  //     name: "パスワード",
+  //   });
+  //   password_textbox.click();
+  //   password_textbox.fill("admin123");
+  await page.getByRole("textbox", { name: "パスワード" }).click();
+  await page.getByRole("textbox", { name: "パスワード" }).fill("admin123");
+  await page
+    .locator("form")
+    .getByRole("button", { name: "サインイン" })
+    .click();
+  await page.waitForTimeout(4000);
+  await page.screenshot({ path: "test-results/auth_after-sign-in.png" });
+  await page.getByRole("button", { name: "User menu" }).click();
+  await expect(page.getByText("admin@example.com")).toBeVisible();
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
+  testIgnore: ["**/generated.spec.ts"], // ここでテスト対象外のファイルを指定
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
