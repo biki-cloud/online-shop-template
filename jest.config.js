818dc5d085805525be8332127bc0094132a1d702
hibiki <hibiki123123@gmail.com>
2025-03-23 07:34:34 +0900

    Refactor CI/CD workflow to increase timeout for E2E-Test job and exclude playwright-report directory from Jest coverage

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     "<rootDir>/.next/",
     "<rootDir>/node_modules/",
     "<rootDir>/e2e/",
+    "<rootDir>/playwright-report/",
   ],
   transform: {
     "^.+\\.(ts|tsx)$": [
